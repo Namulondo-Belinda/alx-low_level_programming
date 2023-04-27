2c2fc13518498aceda1d72836574015972af665b
Namulondo-Belinda <alphabelinda@gmail.com>
2023-04-27 07:41:06 -0700

    Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* main - Test function for positive or negative
+* main - tests function for positive or negative
 * Return: 0
 */
 
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,7 @@
 #ifndef MAIN_H
+
 #define MAIN_H
+
 #include <stdio.h>
 
 void positive_or_negative(int i);
@@ -8,4 +10,3 @@ void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
 #endif /* MAIN_H */
-
